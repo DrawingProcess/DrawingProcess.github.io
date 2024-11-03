12ed85abbea2b6960f0843a6e1120be0770c4e25
DrawingProcess <sjchoi.dp@gmail.com>
2024-11-04 06:45:44 +0900

    Research Profile: Init

---
+++ script.js
@@ -0,0 +1,8 @@
+function expandMisc() {
+    var element = document.getElementsByClassName('collapse-content')[0];
+    if (element.style.display == 'block') {
+        element.style.display = 'none';
+    } else {
+        element.style.display = 'block';
+    }
+}
